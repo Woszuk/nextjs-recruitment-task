ce169717d1c8e7abffa72186cb3ebd456c0e3416
Woszuk <hejnaradrian@gmail.com>
2025-03-23 23:53:05 +0100

    Use user action in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Home page</div>;
+import { getUsers } from "@/app/lib/actions/user-actions";
+
+export default async function Home() {
+  const { users } = await getUsers();
+
+  return (
+    <div className="flex flex-col">
+      {users && users.map((user) => <div key={user.email}>{user.email}</div>)}
+    </div>
+  );
 }
