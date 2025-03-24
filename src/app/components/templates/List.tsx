920432227d3516d4dd751c8f64646cb5620f3d6c
Woszuk <hejnaradrian@gmail.com>
2025-03-25 00:43:35 +0100

    Modify List template

--- src/app/components/templates/List.tsx
+++ src/app/components/templates/List.tsx
@@ -1,12 +1,28 @@
+import Button from "@/app/components/atoms/Button";
+
 type ListProps = {
   children: React.ReactNode;
   title: string;
+  buttonLabel: string;
+  toggleOpen: (e: React.MouseEvent<HTMLElement>) => void;
 };
 
-export default async function List({ children, title }: ListProps) {
+export default function List({
+  children,
+  title,
+  buttonLabel,
+  toggleOpen,
+}: ListProps) {
   return (
-    <div className="flex flex-col gap-3">
-      <p className="text-center text-2xl font-bold uppercase">{title}</p>
+    <div className="flex flex-col gap-5">
+      <div className="relative flex items-center justify-center">
+        <p className="text-2xl font-bold uppercase">{title}</p>
+        <Button
+          className="absolute right-10"
+          label={buttonLabel}
+          onClick={toggleOpen}
+        />
+      </div>
       <div className="grid grid-cols-auto grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-2 font-medium">
         {children}
       </div>
