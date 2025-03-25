23edb25dc6acd9a361f2b94da51a1c0a70156588
Woszuk <hejnaradrian@gmail.com>
2025-03-25 20:41:27 +0100

    Add transparent backdrop for popover

--- src/app/components/atoms/Popover.tsx
+++ src/app/components/atoms/Popover.tsx
@@ -15,11 +15,14 @@ export default function Popover({ children, handleClose, open }: PopoverProps) {
   }
 
   return (
-    <div
-      className="absolute bg-white border-1 px-2 py-1 border-primary-light right-3 top-8 rounded-sm z-1"
-      ref={ref}
-    >
-      {children}
-    </div>
+    <>
+      <div
+        className="absolute bg-white border-1 px-2 py-1 border-primary-light right-3 top-8 rounded-sm z-20"
+        ref={ref}
+      >
+        {children}
+      </div>
+      <div className="fixed inset-0 z-10" />
+    </>
   );
 }
