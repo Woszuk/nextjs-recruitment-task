4114144933ae3777357bfc9d51a79b43632d47f6
Woszuk <hejnaradrian@gmail.com>
2025-03-25 14:15:09 +0100

    Add Dots icon

---
+++ src/app/components/atoms/icons/DotsIcon.tsx
@@ -0,0 +1,13 @@
+export const DotsIcon = (props: React.HTMLAttributes<SVGAElement>) => (
+  <svg
+    className={props.className}
+    fill="currentColor"
+    strokeWidth="0"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    height="1em"
+    width="1em"
+  >
+    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+  </svg>
+);
